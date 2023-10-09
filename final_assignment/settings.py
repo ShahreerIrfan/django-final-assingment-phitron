7690a0e32e27ac7542a9927b8e1532a17962e763
Your Name <mdshahreerirfan@gmail.com>
2023-10-09 23:42:20 +0600

    fdsaf

--- final_assignment/settings.py
+++ final_assignment/settings.py
@@ -83,24 +83,24 @@ WSGI_APPLICATION = 'final_assignment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'I29QpBmiWJFQwDObt6F4',
-        'HOST': 'containers-us-west-31.railway.app',
-        'PORT': '6218',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'I29QpBmiWJFQwDObt6F4',
+#         'HOST': 'containers-us-west-31.railway.app',
+#         'PORT': '6218',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- final_assignment/urls.py
+++ final_assignment/urls.py
@@ -17,5 +17,4 @@ urlpatterns = [
     # path('category/',include('category.urls')),
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
